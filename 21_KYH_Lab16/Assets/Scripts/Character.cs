9c3fae0737ecbf81ed47c2b4e51e27f75e7b31ef
litetor 21 <yeowhwee13@gmail.com>
2021-01-20 19:37:12 +0800

    HW

---
+++ 21_KYH_Lab16/Assets/Scripts/Character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+        }
+    }
+}
